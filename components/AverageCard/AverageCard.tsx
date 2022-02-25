4c53d539a7b0c27e880a7884c6fc364f71a2b295
janitha000 <janitha000@gmail.com>
2022-02-25 23:03:32 +0530

    Fix average card

--- components/AverageCard/AverageCard.tsx
+++ components/AverageCard/AverageCard.tsx
@@ -10,17 +10,17 @@ const AverageCard = () => {
         
       const totalKm = vehicleItems.reduce((a, b) => a + b.usage, 0);
       const totalCost = vehicleItems.map(x => x.amount).reduce((a,b) => a + b,0);
-
+      const average = (totalCost === 0 || totalKm === 0) ? "0" : (totalCost / totalKm).toFixed(2)
       console.log(totalCost)
       console.log(totalKm)
-      return { totalCost, totalKm, average: (totalCost / totalKm).toFixed(2) };
+      return { totalCost, totalKm, average};
     }
   }, [vehicleItems])
 
   return (
     <div className={styles.average}>
       {averages && averages.average && <>
-        <VehicleCard header={"Average cost per km"} value={averages.average} unit={"LKR"} />
+        <VehicleCard header={"Average cost per km"} value={averages.average.toString()} unit={"LKR"} />
         <VehicleCard header={"Total cost"} value={averages.totalCost.toString()} unit={"LKR"} />
         <VehicleCard header={"Total usage"} value={averages.totalKm.toString()} unit={"Km"} /></>}
 
